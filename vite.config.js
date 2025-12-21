b7d791de466dbbf024fba58a7a240e2042dbeb06
Арсений <aenadegnow@gmail.com>
2025-12-21 18:22:19 +0500

    Fix Vite base path for Netlify

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import vitePluginSvgr from "vite-plugin-svgr";
 
 export default defineConfig({
+  base: '/',
   plugins: [
     react(),
     vitePluginSvgr(),
