574fc1b19c4ba876483e4969deb15d0873a20dd6
Арсений <aenadegnow@gmail.com>
2025-12-24 13:04:39 +0500

    feat: add PWA support (manifest, service worker, offline)

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,33 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import vitePluginSvgr from "vite-plugin-svgr";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   base: '/',
   plugins: [
     react(),
     vitePluginSvgr(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "public/icons/coffee_beans.png"
+      ],
+      manifest: {
+        name: "Bean Coffee",
+        short_name: "BeanCoffee",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#603809",
+        icons: [
+          {
+            src: "/public/icons/bean_coffee.png",
+            sizes: "192x192",
+            type: "image/png",
+          }
+        ],
+      }
+    })
   ]
 });
